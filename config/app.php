04a84297a45c28c58767d83223fb9395d0601c87
KJ Coop <git@kjcoop.com>
2026-03-27 00:11:35 -0700

    Rename TOAST_UNDO_DURATION to TOAST_UNDO_DURATION_MS

--- config/app.php
+++ config/app.php
@@ -85,7 +85,7 @@ return [
     'faker_locale' => env('APP_FAKER_LOCALE', 'en_US'),
 
     // Milliseconds the undo toast stays on screen before auto-dismissing
-    'undo_toast_duration' => (int) env('TOAST_UNDO_DURATION', 2000),
+    'undo_toast_duration' => (int) env('TOAST_UNDO_DURATION_MS', 2000),
 
     /*
     |--------------------------------------------------------------------------
