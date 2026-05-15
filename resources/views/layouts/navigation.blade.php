a195ed5b86573e378531aace9c594761cd511d02
KJ Coop <git@kjcoop.com>
2026-05-14 20:29:15 -0700

    Fixing Add New Tag link text on mobile.

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -345,7 +345,7 @@
                     <a href="{{ route('tags.create') }}"
                        class="block w-full ps-3 pe-4 py-2 border-l-4 text-base font-medium text-gray-400 hover:text-gray-100 hover:bg-gray-700 focus:outline-none transition duration-150 ease-in-out"
                        style="border-left-color: transparent">
-                        {{ route('tags.create') }}
+                        Add New Tag
                     </a>
 
                 </div>
