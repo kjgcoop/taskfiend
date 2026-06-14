9f00c36af209f852e38f27eaba5744b4dcc3366c
KJ Coop <git@kjcoop.com>
2026-06-13 23:16:01 -0700

    Quick preview had wonky quotes

--- resources/views/components/task-input-bar.blade.php
+++ resources/views/components/task-input-bar.blade.php
@@ -156,7 +156,7 @@
                  x-transition:enter-start="opacity-0"
                  x-transition:enter-end="opacity-100"
                  class="pl-9 flex flex-wrap items-baseline gap-x-1.5 gap-y-0.5 text-xs">
-                <span class="text-gray-300 break-words min-w-0" x-text="'\u201c' + (preview && preview.title || '') + '\u201d'"></span>
+                <span class="text-gray-300 break-words min-w-0" x-text="'&quot;' + (preview && preview.title || '') + '&quot;'"></span>
                 <template x-if="preview && preview.nodate">
                     <span class="contents">
                         <span class="text-gray-600" aria-hidden="true">·</span>
